9b7583c8b1fead2f00a6140a86b6367af5f988c9
onlywt <1550461814@qq.com>
2022-08-23 23:03:53 +0800

    Update Bootstrap.java

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -14,17 +14,17 @@ public class Bootstrap {
     /**
      * 公众号AppID
      */
-    public static final String APP_ID = "";
+    public static final String APP_ID = "wxd12eb03aa549bf6c";
 
     /**
      * 公众号秘钥
      */
-    public static final String SECRET = "";
+    public static final String SECRET = "c6475f8c61a21f6934c848faa4a94a25";
 
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "";
+    public static final String TEMPLATE_ID = "gURLN9MybrVPb2IQ16Yd0DtUoi1DQ8tXBIah6rm4va0";
 
     /**
      * 初始化
